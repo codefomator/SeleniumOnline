80eb2c2fedab185560142d6cc8e476e919097e2b
codefomator <vikrantprasanth.j@gmail.com>
2025-06-21 01:15:47 +0530

    Updated code1 - driver

--- src/test/java/com/guru99BankingDemo/pageObjects/code1.java
+++ src/test/java/com/guru99BankingDemo/pageObjects/code1.java
@@ -1,5 +1,14 @@
 package com.guru99BankingDemo.pageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class code1 {
+	WebDriver ldriver;
+
+	public code1(WebDriver driver) {
+		ldriver = driver;
+		PageFactory.initElements(driver, this);
+	}
 
 }
